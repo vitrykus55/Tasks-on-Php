7f115500cef51f99481d5647f11a71b72fcd1ad4
vitrykus9 <vitrykus55@gmail.com>
2024-09-19 23:59:08 +0200

    color class
    The `Color` class represents an RGB color with methods to set, get, and mix colors. It allows for random color generation and compares colors for equality. The `mix` method combines two colors by averaging their RGB values and returns a new `Color` instance.

--- index.php
+++ index.php
@@ -1,21 +1,96 @@
 <?php
 
 function is_prime($n)
+class Color
 {
     if ($n < 2) {
         return 'false'; // Числа менші за 2 не є простими
+    private int $red;
+    private int $green;
+    private int $blue;
+
+    public function __construct(int $red, int $green, int $blue)
+    {
+        $this->setRed($red);
+        $this->setGreen($green);
+        $this->setBlue($blue);
+    }
+
+    public function setRed(int $value): void
+    {
+        if ($value >= 0 && $value <= 255) {
+            $this->red = $value;
+        } else {
+            throw new InvalidArgumentException('Red value must be between 0 and 255.');
+        }
+    }
+
+    public function setGreen(int $value): void
+    {
+        if ($value >= 0 && $value <= 255) {
+            $this->green = $value;
+        } else {
+            throw new InvalidArgumentException('Green value must be between 0 and 255.');
+        }
     }
 
     for ($i = 2; $i < $n; $i++) {
         if ($n % $i == 0) {
             return 'false'; // Знайдено дільник, число не просте
+    public function setBlue(int $value): void
+    {
+        if ($value >= 0 && $value <= 255) {
+            $this->blue = $value;
+        } else {
+            throw new InvalidArgumentException('Blue value must be between 0 and 255.');
         }
     }
 
     return 'true'; // Дільників не знайдено, число просте
+    public function getRed(): int
+    {
+        return $this->red;
+    }
+
+    public function getGreen(): int
+    {
+        return $this->green;
+    }
+
+    public function getBlue(): int
+    {
+        return $this->blue;
+    }
+
+    public function equals(Color $color): bool
+    {
+        return $color->getRed() === $this->red &&
+            $color->getGreen() === $this->green &&
+            $color->getBlue() === $this->blue;
+    }
+
+    public function random(): void
+    {
+        $this->red = random_int(0, 255);
+        $this->green = random_int(0, 255);
+        $this->blue = random_int(0, 255);
+    }
+
+    public function mix(Color $color): Color
+    {
+        $mixedRed = ($this->red + $color->getRed()) / 2;
+        $mixedGreen = ($this->green + $color->getGreen()) / 2;
+        $mixedBlue = ($this->blue + $color->getBlue()) / 2;
+
+        return new Color((int)$mixedRed, (int)$mixedGreen, (int)$mixedBlue);
+    }
 }
 
 echo is_prime(7) . "\n";  // true
 echo is_prime(18) . "\n"; // false
 echo is_prime(11) . "\n"; // true
-echo is_prime(178) . "\n"; // false
+echo is_prime(178) . "\n"; // false$color1 = new Color(250, 250, 250);
+$color2 = new Color(100, 100, 100);
+$mixedColor = $color1->mix($color2);
+
+echo "Mixed Color - Red: " . $mixedColor->getRed() . ", Green: " . $mixedColor->getGreen() . ", Blue: " . $mixedColor->getBlue();
