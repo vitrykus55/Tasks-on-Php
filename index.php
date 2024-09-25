20563792c75565367c4bcfc027212469f884a08a
vitrykus9 <vitrykus55@gmail.com>
2024-09-25 15:24:19 +0200

    bug fixes and improvements to the Product class
    - Fixed isAvailable() method name (was isAvalible()).
    - Corrected the logic of the increaseQuantity() method to correctly increase the quantity of the product.
    - Removed repetition of the total value output in the displayProductInfo() method.
    - Added typing for parameters of increaseQuantity() and decreaseQuantity() methods.
    - Added line breaks to improve output readability.

--- index.php
+++ index.php
@@ -1,17 +1,70 @@
-<!--#2-->
-
 <?php
-$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-$n = 3;
 
-$result = [];
-
-for ($i = 0; $i < count($arr); $i += $n)
+class Product
 {
-    $result[]= array_slice($arr, $i, $n);
-}
-echo '<pre>';
-print_r($result);
-echo '</pre>';
+    public string $name;
+    public int $price;
+    public int $quantity;
 
-?>
+    public function __construct(string $name, int $price, int $quantity)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->quantity = $quantity;
+    }
+
+    public function totalCost(): int
+    {
+        return $this->price * $this->quantity;
+    }
+
+    public function applyDiscount(float $discount_percentage): void
+    {
+        $this->price -= $this->price * ($discount_percentage / 100);
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    public function isAvailable(): bool
+    {
+        return $this->quantity > 0;
+    }
+
+    public function increaseQuantity(int $amount): void
+    {
+        $this->quantity += $amount;
+    }
+
+    public function decreaseQuantity(int $amount): void
+    {
+        if ($this->quantity >= $amount) {
+            $this->quantity -= $amount;
+        } else {
+            echo 'Not enough quantity<br>';
+        }
+    }
+
+    public function displayProductInfo(): void
+    {
+        echo 'Name: ' . $this->name . '<br>';
+        echo 'Price: ' . $this->price . '<br>';
+        echo 'Quantity: ' . $this->quantity . '<br>';
+        echo 'Total cost: ' . $this->totalCost() . '<br>';
+        echo $this->isAvailable() ? 'We have this<br>' : 'We don\'t have this<br>';
+    }
+}
+
+$product = new Product("Phone", 10000, 511);
+$product->displayProductInfo();
+
+$product->applyDiscount(10);
+echo "Price after discount: " . $product->getPrice() . "<br>";
+
+$product->increaseQuantity(10);
+$product->displayProductInfo();
+
+$product->decreaseQuantity(5);
+$product->displayProductInfo();
