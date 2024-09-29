2361186e5d909e446643fc153f83a5c5e2b49623
vitrykus9 <vitrykus55@gmail.com>
2024-09-29 12:55:28 +0200

    Factory Method
    add delivery type for taxi services with economy standard, and luxury models

--- index.php
+++ index.php
@@ -1,17 +1,123 @@
-<!--#2-->
-
 <?php
-$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-$n = 3;
 
-$result = [];
-
-for ($i = 0; $i < count($arr); $i += $n)
+interface Car
 {
-    $result[]= array_slice($arr, $i, $n);
-}
-echo '<pre>';
-print_r($result);
-echo '</pre>';
+    public function getModel(): string;
 
-?>
+    public function getPrice(): float;
+}
+
+class EconomyCar implements Car
+{
+    private string $model;
+    private float $price;
+
+    public function __construct()
+    {
+        $this->model = 'Toyota Yaris';
+        $this->price = 110;
+    }
+
+    public function getModel(): string
+    {
+        return $this->model;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+}
+
+class StandardCar implements Car
+{
+    private string $model;
+    private float $price;
+
+    public function __construct()
+    {
+        $this->model = 'Volkswagen Golf VIII';
+        $this->price = 160;
+    }
+
+    public function getModel(): string
+    {
+        return $this->model;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+}
+
+class LuxuryCar implements Car
+{
+    private string $model;
+    private float $price;
+
+    public function __construct()
+    {
+        $this->model = 'Mercedes Maybach';
+        $this->price = 240;
+    }
+
+    public function getModel(): string
+    {
+        return $this->model;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+}
+
+abstract class TaxiFactory
+{
+    abstract public function createCar(): Car;
+}
+
+class EconomyTaxiFactory extends TaxiFactory
+{
+    public function createCar(): Car
+    {
+        return new EconomyCar();
+    }
+}
+
+class StandardTaxiFactory extends TaxiFactory
+{
+    public function createCar(): Car
+    {
+        return new StandardCar();
+    }
+}
+
+class LuxuryTaxiFactory extends TaxiFactory
+{
+    public function createCar(): Car
+    {
+        return new LuxuryCar();
+    }
+}
+
+function displayCarDetails(TaxiFactory $factory)
+{
+    $car = $factory->createCar();
+    echo "Модель: " . $car->getModel() . "<br>";
+    echo "Ціна: ₴" . $car->getPrice() . "<br>";
+}
+
+$economyFactory = new EconomyTaxiFactory();
+$standardFactory = new StandardTaxiFactory();
+$luxuryFactory = new LuxuryTaxiFactory();
+
+echo "Economy taxi:<br>";
+displayCarDetails($economyFactory);
+
+echo "<br>Standard taxi:<br>";
+displayCarDetails($standardFactory);
+
+echo "<br>Luxury taxi:<br>";
+displayCarDetails($luxuryFactory);
