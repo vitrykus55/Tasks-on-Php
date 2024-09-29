ac962a89f596930b516c6eecb0df5b3ee072c921
vitrykus9 <vitrykus55@gmail.com>
2024-09-29 12:34:11 +0200

    sql patern
    you can use it for sql conectioin just use a class DataBaseConnection

--- index.php
+++ index.php
@@ -1,17 +1,66 @@
-<!--#2-->
-
 <?php
-$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-$n = 3;
 
-$result = [];
-
-for ($i = 0; $i < count($arr); $i += $n)
+class DataBaseConnection
 {
-    $result[]= array_slice($arr, $i, $n);
-}
-echo '<pre>';
-print_r($result);
-echo '</pre>';
+    private static $instance = null;
+    private $connection;
 
-?>
+    private function __construct()
+    {
+        $servername = "localhost";
+        $username = "root";
+        $password_db = "root";
+        $dbname = "logini_db";
+        $port = 3306;
+        $socket = '/Applications/MAMP/tmp/mysql/mysql.sock';
+
+        try {
+            $this->connection = new mysqli($servername, $username, $password_db, $dbname, $port, $socket);
+
+            if ($this->connection->connect_error) {
+                throw new Exception("Connection failed: " . $this->connection->connect_error);
+            }
+        } catch (Exception $e) {
+            $error = $e->getMessage();
+            echo 'Error: ' . $error;
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new DataBaseConnection();
+        }
+        return self::$instance;
+    }
+
+    public function query($sql)
+    {
+        return $this->connection->query($sql);
+    }
+
+    public function prepare($sql)
+    {
+        return $this->connection->prepare($sql);
+    }
+}
+
+$db = DataBaseConnection::getInstance();
+$result = $db->query("SELECT * FROM user");
+
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        echo $row['name'] . " - " . $row['email'] . "<br>";
+    }
+}
+
+$db2 = DataBaseConnection::getInstance();
+$statement = $db2->prepare("SELECT * FROM user WHERE email = ?");
+$email = 'test@example.com';  
+$statement->bind_param('s', $email);
+$statement->execute();
+$user_result = $statement->get_result();
+
+while ($user = $user_result->fetch_assoc()) {
+    echo $user['name'] . " - " . $user['email'] . "<br>";
+}
